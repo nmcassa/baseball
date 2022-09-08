9f3e30f90183f7e22f63ad325212271559c500ea
Nicholas Cassarino <ncassari@uncc.edu>
2022-09-08 13:24:41 -0400

    refactor

--- game.py
+++ game.py
@@ -6,60 +6,35 @@ from bs4 import BeautifulSoup
 from json import JSONEncoder
 
 class Game: 
-	def __init__(self, date: str, home: str) -> None:
-		self.home_abb = home
-		self.date = date
-		self.url = "https://www.baseball-reference.com/previews/2022/" + self.home_abb + self.date + "0.shtml"
-
-		page = self.get_parsed_page(self.url)
-
-		self.away = self.get_away(page)
-
-	def get_parsed_page(self, url: str) -> None:
-		headers = {
-			"referer": "https://baseball-reference.com",
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-		}
-
-		return BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
+	def __init__(self, url: str) -> None:
+		print("non")
 
 	def jsonify(self) -> str:
 		return json.dumps(self, indent=4,cls=Encoder)
 
-	def get_away(self, page: None) -> str:
-		data = page.find("h1").text.split(" ")
-		self.home = data[1]
-		return data[4]
-
-#returns all the teams playing, away team first.
-def get_all_game_days() -> list:
+def get_parsed_page(url: str) -> None:
 	headers = {
 		"referer": "https://baseball-reference.com",
 		"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
 	}
 
-	page = BeautifulSoup(requests.get("https://www.mlb.com/scores", headers=headers).text, "lxml")
-	
-	games = page.find_all("div", {'data-test-mlb': ['singleGameContainer'], })
+	return BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
 
-	ret = []
+#returns all the urls previews for all the days games
+def get_all_game_days() -> list:
+	page = get_parsed_page("https://www.baseball-reference.com/previews/")
+	data = page.findAll("a", text = "Preview")
 
-	for game in games:
-		teams = game.find_all("div", {'class': ['TeamWrappersstyle__MobileTeamWrapper-sc-uqs6qh-1'], })[2:]
-		for team in teams:
-			ret.append(team.text)
+	urls = []
 
-	return ret
+	for data_point in data:
+		urls.append(data_point['href'])
 
-def get_date() -> str:
-	x = date.today()
-	return str(x).replace('-', '')
+	return urls
 
 class Encoder(JSONEncoder):
 	def default(self, o):
 		return o.__dict__
 
 if __name__ == "__main__":
-	today = Game(get_date(), "CLE")
-	print(today.jsonify())
-	#print(today.away_team)
+	print(get_all_game_days())
